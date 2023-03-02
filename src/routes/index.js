81ec649e57e2e0887d2c16b6fbad054ab51704f9
Minh8x8 <105042689+Minh8x8@users.noreply.github.com>
2023-03-02 22:17:55 +0700

    delete unnecessary files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,7 @@
-const newsRouter = require('./news');
 const meRouter = require('./me');
 const siteRouter = require('./site');
-const coursesRouter = require('./courses');
 const toyRouter = require('./toy');
 function route(app) {
-    app.use('/search', newsRouter);
-    app.use('/courses', coursesRouter);
     app.use('/me', meRouter);
     app.use('/toys', toyRouter);
     app.use('/', siteRouter);
