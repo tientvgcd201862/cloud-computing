a79d14d6afb6df2d1329f1b1d10c185e62d33625
tien <tongviettien1902@gmail.com>
2023-03-04 19:55:12 +0700

    commit

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://atnstore:atnstore@atnstore.qlgzhdp.mongodb.net/atn_store?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://tienbippro:tienbippro@atnstore.5u33ngn.mongodb.net/atn_store?retryWrites=true&w=majority', {
             useNewUrlParser : true,
             useUnifiedTopology: true,
         });
-        console.log('connect successfully');
+        console.log('connect successful');
     } catch (error) {
         console.log('connect failed!!!');
     }
